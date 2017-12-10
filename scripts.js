e68e0a1257764d9e33e4fdc9cbb32fd6404cb64c
Julita <33807278+Julita45@users.noreply.github.com>
2017-12-10 15:10:26 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+	console.log('DOM loaded - you can have fun');
+});
+
 $('span').each(function(index, element) {
 	if (index % 2 == 0) {
 		$(element).css('color', 'red');
@@ -9,6 +13,6 @@ $('p').each(function(index, element) {
 	$(element).append(button)
 });
 
-$('button').click(function() {
+$('button').on(function() {
 	alert($(this).attr('data-tmp'));
 });
